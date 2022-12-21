ea62859f6b29a318315df59f3d29969d898d7ccd
SouvikRout25 <souvikrout.21@gmail.com>
2022-12-21 12:59:14 +0530

    done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pizzaAppFrontend'`, () => {
+  it(`should have as title 'pizzaApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pizzaAppFrontend');
+    expect(app.title).toEqual('pizzaApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pizzaAppFrontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pizzaApp app is running!');
   });
 });
